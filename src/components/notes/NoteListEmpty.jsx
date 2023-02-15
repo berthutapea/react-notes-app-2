dfef51468695a9be2d72e0df657b9c4d26292741
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-16 03:12:54 +0700

    PropTypes

--- src/components/notes/NoteListEmpty.jsx
+++ src/components/notes/NoteListEmpty.jsx
@@ -1,9 +1,9 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function NoteListEmpty() {
-//   return (
-//     <section className="notes-list-empty">
-//       <p className="notes-list__empty">No notes here.</p>
-//     </section>
-//   )
-// }
+export default function NoteListEmpty() {
+  return (
+    <section className="notes-list-empty">
+      <p className="notes-list__empty">No notes here.</p>
+    </section>
+  )
+}
