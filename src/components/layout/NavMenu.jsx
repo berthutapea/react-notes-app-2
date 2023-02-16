a38b3289f4b642e1ad4be7ab909b989b25b12534
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-16 23:33:23 +0700

    Navigation

--- src/components/layout/NavMenu.jsx
+++ src/components/layout/NavMenu.jsx
@@ -1,16 +1,16 @@
-// import React from 'react'
-// import { Link, useLocation } from 'react-router-dom'
-// import ThemeToggler from './ThemeToggler'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import ThemeToggler from './ThemeToggler'
 
-// export default function NavMenu() {
-//   const { pathname } = useLocation()
-//   return (
-//     <nav className="navigation">
-//       <ul>
-//         <li>
-//           {pathname !== '/archives'
-//             ? <Link to="/archives" title="Archives">Archive</Link>
-//             : <Link to="/" title="Home">Home</Link>}
+export default function NavMenu() {
+  const { pathname } = useLocation()
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          {pathname !== '/archives'
+            ? <Link to="/archives" title="Archives">Archive</Link>
+            : <Link to="/" title="Home">Home</Link>}
 
         </li>
         <li>
