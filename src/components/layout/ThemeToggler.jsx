1e2a56c658342eed2013713e143eabb1de4bde1a
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-17 23:08:04 +0700

    PropTypes

--- src/components/layout/ThemeToggler.jsx
+++ src/components/layout/ThemeToggler.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { capitalizeFirstLetter } from '../../utils'
+// import React, { useEffect, useState } from 'react'
+// import { capitalizeFirstLetter } from '../../utils'
 
-function ThemeToggler() {
-  const [theme, setTheme] = useState('dark')
+// function ThemeToggler() {
+//   const [theme, setTheme] = useState('dark')
 
-  const changeTheme = (val) => {
-    setTheme(val)
-    const root = window.document.documentElement
-    root.setAttribute('data-theme', val)
-    localStorage.setItem('theme', val)
-  }
+//   const changeTheme = (val) => {
+//     setTheme(val)
+//     const root = window.document.documentElement
+//     root.setAttribute('data-theme', val)
+//     localStorage.setItem('theme', val)
+//   }
 
-  useEffect(() => {
-    if (localStorage.theme) {
-      changeTheme(localStorage.theme)
-    } else {
-      localStorage.setItem('theme', 'dark')
-      changeTheme('dark')
-    }
-  }, [])
+//   useEffect(() => {
+//     if (localStorage.theme) {
+//       changeTheme(localStorage.theme)
+//     } else {
+//       localStorage.setItem('theme', 'dark')
+//       changeTheme('dark')
+//     }
+//   }, [])
 
-  return (
-    <button
-      type="button"
-      onClick={() => changeTheme(theme === 'dark' ? 'light' : 'dark')}
-    >
-      {capitalizeFirstLetter(theme)}
-      {' '}
-      Theme
-    </button>
-  )
-}
+//   return (
+//     <button
+//       type="button"
+//       onClick={() => changeTheme(theme === 'dark' ? 'light' : 'dark')}
+//     >
+//       {capitalizeFirstLetter(theme)}
+//       {' '}
+//       Theme
+//     </button>
+//   )
+// }
 
-export default ThemeToggler
+// export default ThemeToggler
