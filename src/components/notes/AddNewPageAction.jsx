6b834d246c31278117f4a74cee6176283168c2c4
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-17 23:05:30 +0700

    PropTypes

--- src/components/notes/AddNewPageAction.jsx
+++ src/components/notes/AddNewPageAction.jsx
@@ -1,38 +1,38 @@
-// import React from 'react'
-// import PropTypes from 'prop-types'
-// import { HiX, HiCheck } from 'react-icons/hi'
-// import { useNavigate } from 'react-router-dom'
-// import PageAction from '../layout/PageAction'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { HiX, HiCheck } from 'react-icons/hi'
+import { useNavigate } from 'react-router-dom'
+import PageAction from '../layout/PageAction'
 
-// function AddNewPageAction({ handleSave }) {
-//   const navigate = useNavigate()
+function AddNewPageAction({ handleSave }) {
+  const navigate = useNavigate()
 
-//   return (
-//     <PageAction page="add-new-page">
-//       <>
-//         <button
-//           className="action"
-//           type="button"
-//           title="Close"
-//           onClick={() => navigate('/')}
-//         >
-//           <HiX />
-//         </button>
-//         <button
-//           className="action"
-//           type="button"
-//           title="Add"
-//           onClick={() => handleSave()}
-//         >
-//           <HiCheck />
-//         </button>
-//       </>
-//     </PageAction>
-//   )
-// }
+  return (
+    <PageAction page="add-new-page">
+      <>
+        <button
+          className="action"
+          type="button"
+          title="Close"
+          onClick={() => navigate('/')}
+        >
+          <HiX />
+        </button>
+        <button
+          className="action"
+          type="button"
+          title="Add"
+          onClick={() => handleSave()}
+        >
+          <HiCheck />
+        </button>
+      </>
+    </PageAction>
+  )
+}
 
 AddNewPageAction.propTypes = {
   handleSave: PropTypes.func.isRequired
 }
 
-// export default AddNewPageAction
+export default AddNewPageAction
